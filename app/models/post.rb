da735b1801b2e137f23e268e228346ad9447340a
takurofukuta <mojuraspax@icloud.com>
2021-02-25 17:07:34 +0900

    モデルにバリデーションを追加

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,6 @@
 class Post < ApplicationRecord
+  # ***** 以下を追加 *****
+  validates :title, presence: true, length: { maximum: 50 }
+  validates :content, presence: true, length: { maximum: 500 }
+  # ***** 以上を追加 *****
 end
